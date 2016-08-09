66cc7c94a2a70ee7ffd81fb4b2ba40b7938bd7c0
Marcin Mazur <marmaz.966@o2.pl>
2016-08-09 18:17:08 +0200

    Dodano sceny dla farmy i przejścia między nimi

---
+++ Assets/Scripts/SwitchBetweenScenes.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchBetweenScenes : MonoBehaviour {
+
+	public string Destination;
+
+	public void LoadScene()
+	{
+		Application.LoadLevel (Destination);
+	}
+}
