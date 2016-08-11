96db12f0bcf25f1e1ddc42e866a4eb7cee9be778
Mateusz <Mateusz@LAMALAP>
2016-08-11 09:37:46 +0200

    Added events for enemy death.

---
+++ Assets/WolfGroupManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class WolfGroupManager : MonoBehaviour
+{
+
+    int wolvesCounter;
+
+    // Use this for initialization
+    void Start()
+    {
+        var enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        wolvesCounter = enemies.Length;
+        foreach (var item in enemies)
+        {
+            Assets.LogicSystem.Events.Instance.RegisterForEvent(item.name, x => OnWolfDeath());
+        }
+    }
+
+
+
+    public void OnWolfDeath()
+    {
+        wolvesCounter--;
+        if (wolvesCounter == 0)
+        {
+            Assets.LogicSystem.Events.Instance.DispatchEvent("GameWon", null);
+            Debug.Log("Battle won");
+        }
+    }
+}
