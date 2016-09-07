f14c7db0340327bcefbccaf8d63000120a2bd019
Marcin Mazur <marmaz.966@o2.pl>
2016-09-07 16:55:36 +0200

    Placeholderowe ekrany sklepu i szopy.

---
+++ Assets/Scripts/UIScripts/SetEnabled.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetEnabled : MonoBehaviour {
+
+    public GameObject screenToView;
+    public GameObject[] screensToHide;
+
+
+	public void TurnOnScreen(){
+        screenToView.active = true;
+        Debug.Log("Enabling" + screenToView.name);
+
+        foreach (GameObject item in screensToHide)
+            item.active = false;
+	}
+}
