6bf5db79066dc59ccd59c1ea261bcca836dece3f
Mateusz <Mateusz@LAMALAP>
2016-09-18 18:35:12 +0200

    Created double attack skill

---
+++ Assets/Scripts/Skills/DoubleAttack.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName ="Double Attack", menuName ="Game/Skills/Double Attack")]
+public class DoubleAttack : Skill {
+
+    public int AttackCount = 2;
+    public override void Initialize(GameObject parent)
+    {
+        base.Initialize(parent);
+    }
+
+    protected override void PerformAction(GameObject actor, GameObject target)
+    {
+        Debug.Log(actor.name + " attacks " + target.name + " " + AttackCount + " times.");
+        for (int i = 0; i < AttackCount; i++)
+        {
+            target.GetComponent<Assets.Scripts.Interfaces.IReciveDamage>().DealDamage(Power);
+        }
+        base.PerformAction(actor, target);
+    }
+}
