6d5e7cbd781535d399ca689ca5c410a529b82302
Mateusz <Mateusz@LAMALAP>
2016-09-30 11:10:39 +0200

    Added rouge's posion

---
+++ Assets/Scripts/ScriptableObjects/Brains/PoisonedBrain.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+
+namespace Assets.Scripts.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "Poisoned Brain", menuName = "Game/Brains/Poisoned Brain")]
+    public class PoisonedBrain : AbstractBrain
+    {
+
+        public int Damage { get; set; }
+
+        public void SetDuration(int duration)
+        {
+            Duration = duration;
+        }
+
+
+        public override void Initialize(GameObject[] targets)
+        {
+
+        }
+
+        public override void Think(GameObject parent)
+        {
+            Debug.Log(parent.name + " takes " + Damage + " damage from poison.");
+            parent.GetComponent<Interfaces.IReciveDamage>().DealDamage(Damage);
+            base.Think(parent);
+        }
+
+    }
+}
+
