c601721c86bcacd484d74e37eb9b5cfef795d01e
Mateusz <Mateusz@LAMALAP>
2016-10-08 14:21:44 +0200

    Adjusted barber's window for sheep data

---
+++ Assets/Scripts/BarberCanvasScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BarberCanvasScript : MonoBehaviour
+{
+
+    const int SheepCounter = 4;
+
+    public Slider[] sliders;
+    public Text[] Names;
+    public SheepData[] sheepData;
+    public Text[] SlidersValue;
+    public Text[] MaxWoolText;
+
+
+    void Start()
+    {
+        for (int i = 0; i < SheepCounter; i++)
+        {
+            sliders[i].wholeNumbers = true;
+            sliders[i].maxValue = sheepData[i].Wool;
+            sliders[i].minValue = 0;
+            sliders[i].value = sheepData[i].Wool;
+            Names[i].text = sheepData[i].Name;
+            MaxWoolText[i].text = sheepData[i].Wool.ToString();
+            
+        }
+    }
+
+    public void UpdateSliderText(int number)
+    {
+        SlidersValue[number].text = sliders[number].value.ToString();
+    }
+
+
+
+}
