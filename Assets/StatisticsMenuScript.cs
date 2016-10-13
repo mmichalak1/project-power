5840c578be478a6cb47144b2cce47cfdc81c4761
Jakub Witoński <jakub.witonski@gmail.com>
2016-10-13 18:17:11 +0200

    Sorry

--- Assets/StatisticsMenuScript.cs
+++ Assets/StatisticsMenuScript.cs
@@ -5,7 +5,10 @@ public class StatisticsMenuScript : MonoBehaviour {
 
     public SheepData[] SheepData;
 
-    public Text Name, Attack, HP, WoolCapacity, Defence, Wool;
+    public Text Name, Attack, HP, WoolCapacity, Defence, Defence_Bonus_Counter, Wool, Wool_Cap;
+    public Image[] SheepIcon;
+    public static Color pushed = new Color(0.83F,0.83F,0.83F,1.0F);
+    public static Color unpushed = new Color(0.73F, 0.73F, 0.73F, 1.0F);
 
     void Start()
     {
@@ -20,6 +23,19 @@ public class StatisticsMenuScript : MonoBehaviour {
         HP.text = data.BasicMaxHealth.ToString();
         WoolCapacity.text = data.MaxWool.ToString();
         Defence.text = data.Defence.ToString();
-        Wool.text = data.Wool.ToString() + "/" + data.MaxWool.ToString();
+        Defence_Bonus_Counter.text = "+ " + data.Wool.ToString();
+        Wool.text = data.Wool.ToString();
+        Wool_Cap.text = "/" + data.MaxWool.ToString();
+        ColorChange(SheepNumber);
+    }
+
+    public void ColorChange(int SheepNumber)
+    {
+        foreach (Image image in SheepIcon)
+        {
+            image.color = unpushed;
+        }
+        SheepIcon[SheepNumber].color = pushed;
+
     }
 }
