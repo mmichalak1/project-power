ed9c2c4e8f378b77b097a62826a1f8432f01b9bd
Jakub Witoński <jakub.witonski@gmail.com>
2016-10-17 01:30:27 +0200

    Added Skills and statistic UI in Farm-Gym.

--- Assets/SkillsMenuScript.cs
+++ Assets/SkillsMenuScript.cs
@@ -6,10 +6,16 @@ using UnityEngine.UI;
 public class SkillsMenuScript : MonoBehaviour {
 
     public SheepData[] SheepData;
+    public int SheepNumber;
 
     public Image[] SheepIcon;
 
     public Button[] SheepButton;
+
+    public GameObject SkillDesc;
+    public Text Name;
+    public Text Description;
+    public Image Icon;
 
     public static Color pushed = new Color(0.83F, 0.83F, 0.83F, 1.0F);
     public static Color unpushed = new Color(0.73F, 0.73F, 0.73F, 1.0F);
@@ -21,6 +27,7 @@ public class SkillsMenuScript : MonoBehaviour {
 
     public void LoadData(int SheepNumber)
     {
+        this.SheepNumber = SheepNumber;
         for(int i = 0; i<4; i++)
         {
             SheepButton[i].image.sprite = SheepData[SheepNumber].SheepSkills.Skills[i].Icon;
@@ -36,5 +43,17 @@ public class SkillsMenuScript : MonoBehaviour {
         }
         SheepIcon[SheepNumber].color = pushed;
 
+    }
+    public void ButtonClick(int SheepButton)
+    {
+        SkillDesc.SetActive(true);
+        Name.text = SheepData[this.SheepNumber].SheepSkills.Skills[SheepButton].Name;
+        Description.text = SheepData[this.SheepNumber].SheepSkills.Skills[SheepButton].Description;
+        Icon.sprite = SheepData[this.SheepNumber].SheepSkills.Skills[SheepButton].Icon;
+    }
+
+    public void ButtonExit()
+    {
+        SkillDesc.SetActive(false);
     }
 }
