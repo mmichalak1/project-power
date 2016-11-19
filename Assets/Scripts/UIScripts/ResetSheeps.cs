3e4ce3033352e76a0ddcce0b8779947b28fc1fe2
Jakub Witoński <jakub.witonski@gmail.com>
2016-11-20 00:03:34 +0100

    Added ResetSheep button in Farm.

---
+++ Assets/Scripts/UIScripts/ResetSheeps.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetSheeps : MonoBehaviour {
+
+    public EntityData[] SheepData;
+    public EntityData[] SheepDataBackup;
+    public WoolUpdater woolUpdater;
+    public LevelData[] levelData;
+
+    [SerializeField]
+    private WoolCounter DefaultWoolCounter;
+
+    public void Reset()
+    {
+        DefaultWoolCounter.WoolCount = 0;
+        woolUpdater.UpdateWoolView();
+
+        foreach (LevelData item in levelData)
+        {
+            item.Progress = 0;
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            SheepData[i].BasicAttack = SheepDataBackup[i].BasicAttack;
+            SheepData[i].BasicMaxHealth = SheepDataBackup[i].BasicMaxHealth;
+            SheepData[i].BasicDefence = SheepDataBackup[i].BasicDefence;
+            SheepData[i].Level = SheepDataBackup[i].Level;
+            SheepData[i].Experience = SheepDataBackup[i].Experience;
+            SheepData[i].ExperienceForNextLevel = SheepDataBackup[i].ExperienceForNextLevel;
+            SheepData[i].Wool = SheepDataBackup[i].Wool;
+            SheepData[i].MaxWool = SheepDataBackup[i].MaxWool;
+            SheepData[i].WoolGrowth = SheepDataBackup[i].WoolGrowth;
+            for (int j = 0; j < 4; j++)
+            {
+                if(j != 1) // for class skill
+                    SheepData[i].SheepSkills.Skills[j].IsActive = false;
+            }
+            SheepData[i].OffensiveItem = null;
+            SheepData[i].DefensiveItem = null;
+        }
+    }
+}
