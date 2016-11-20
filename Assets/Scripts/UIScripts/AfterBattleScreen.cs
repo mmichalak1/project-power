910404291a079c18c7b3d49407fa4821c9692c87
Marcin Mazur <marmaz.966@o2.pl>
2016-11-20 11:30:04 +0100

    Fixed wool indicator on after battle screen

--- Assets/Scripts/UIScripts/AfterBattleScreen.cs
+++ Assets/Scripts/UIScripts/AfterBattleScreen.cs
@@ -8,13 +8,17 @@ public class AfterBattleScreen : MonoBehaviour {
     private WoolCounter Counter;
     private int woolForFight;
     private int woolToDisplay;
+    private int woolGainedDisplay;
     private int condition;
     private int step;
     private float delta = 0;
 
     public Text WoolGrowIndicator;
+    public Text WoolGained;
     public int stepFactor;
     public float delay;
+    public float waitTime;
+    private State state;
 
 	void Start () {
         Assets.LogicSystem.Events.Instance.RegisterForEvent("AfterBattleScreen", OnEvoke);
@@ -23,23 +27,50 @@ public class AfterBattleScreen : MonoBehaviour {
 	
 	void Update () {
 
+        switch (state)
+        {
+            case State.Play:
+                {
+                    Play();
+                } break;
+            case State.Wait:
+                {
+                    StartCoroutine(Wait(waitTime));
+                }break;
+            default:
+                break;
+        }
+
+	}
+
+    private IEnumerator Wait(float time)
+    {
+        yield return new WaitForSeconds(time);
+        state = State.Play;
+    }
+
+    private void Play()
+    {
         delta += Time.deltaTime;
-        if (delta >= delay )
+        if (delta >= delay)
         {
             if (woolToDisplay + step < condition)
             {
                 woolToDisplay += step;
+                woolGainedDisplay -= step;
                 WoolGrowIndicator.text = woolToDisplay.ToString();
+                WoolGained.text = " + " + woolGainedDisplay.ToString();
             }
             else
             {
                 woolToDisplay = condition;
+                woolGainedDisplay = 0;
                 WoolGrowIndicator.text = woolToDisplay.ToString();
+                WoolGained.text = " + " + woolGainedDisplay.ToString();
             }
             delta = 0;
         }
-
-	}
+    }
 
     public void ContinueButton()
     {
@@ -54,9 +85,18 @@ public class AfterBattleScreen : MonoBehaviour {
         WoolGrowIndicator.text = Counter.WoolCount.ToString();
         woolForFight = (int)obj;
         woolToDisplay = Counter.WoolCount;
+        woolGainedDisplay = woolForFight;
+        WoolGained.text = " + " + woolGainedDisplay.ToString();
         condition = woolToDisplay + woolForFight;
         step = (int)(woolForFight / stepFactor);
+        state = State.Wait;
         if (step == 0)
             step = 1;
     }
+
+    private enum State
+    {
+        Play,
+        Wait
+    };
 }
