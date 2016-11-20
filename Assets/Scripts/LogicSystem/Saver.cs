f0f93bd30a4994b874503994e65c8f3c2834c7c0
Mateusz <Mateusz@LAMALAP>
2016-11-20 01:56:43 +0100

    stuff

--- Assets/Scripts/LogicSystem/Saver.cs
+++ Assets/Scripts/LogicSystem/Saver.cs
@@ -5,10 +5,14 @@ using System.IO;
 public static class Saver
 {
     private static string FILENAME = "Save.dat";
+
+
+
+
     public static GameSaveData Load()
     {
         string path = Application.persistentDataPath + FILENAME;
-    GameSaveData state;
+        GameSaveData state;
         if (!File.Exists(path))
             return null;
         using (var stream = File.Open(path, FileMode.Open))
