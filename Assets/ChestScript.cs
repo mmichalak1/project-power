e004b383d190c7853699ec674e04b6abccbe03b8
Mateusz <Mateusz@LAMALAP>
2016-11-21 19:36:17 +0100

    Added wool giving chest

---
+++ Assets/ChestScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class ChestScript : MonoBehaviour {
+
+    public GameObject Player;
+    public int WoolForChest = 10;
+    public UnityEvent OnPlayerEnter;
+
+
+    private bool isDetecting = true;
+
+	void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == Player && isDetecting)
+        {
+            isDetecting = !isDetecting;
+            OnPlayerEnter.Invoke();
+        }
+    }
+}
