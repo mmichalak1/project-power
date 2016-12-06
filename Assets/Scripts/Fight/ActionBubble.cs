130a8f18fb5ec22570190c7b39338f33c5643010
Jakub Witoński <jakub.witonski@gmail.com>
2016-12-06 20:35:48 +0100

    Now acctionBubble show only "!".

--- Assets/Scripts/Fight/ActionBubble.cs
+++ Assets/Scripts/Fight/ActionBubble.cs
@@ -3,26 +3,6 @@ using UnityEngine.UI;
 
 public class ActionBubble : MonoBehaviour {
 
-    public Image[] skillImage;
-    private Skill[] skills = new Skill[3];
-    private int whichIcon = 0;
-    public Sprite imageForNull;
-    public void SetSkill(Skill skill)
-    {
-        if(whichIcon<3)
-        {
-            skillImage[whichIcon].sprite = skill.Icon;
-            skills[whichIcon] = skill;
-            whichIcon++;
-        }
-        
-    }
-    public void Clear()
-    {
-        whichIcon = 0;
-        foreach (Image item in skillImage)
-            item.sprite = imageForNull;
-    }
     public void TurnOn()
     {
         gameObject.SetActive(true);
@@ -30,7 +10,6 @@ public class ActionBubble : MonoBehaviour {
     }
     public void TurnOff()
     {
-        Clear();
         gameObject.SetActive(false);
         //Debug.Log("Disabling" + this.gameObject.name);
     }
