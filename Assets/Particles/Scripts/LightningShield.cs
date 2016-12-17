27285e1b7788fe055c90c1b5ebcaeb8f66007444
Marcin Mazur <marmaz.966@o2.pl>
2016-12-17 12:56:47 +0100

    Added effect to lightning shield

---
+++ Assets/Particles/Scripts/LightningShield.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightningShield : MonoBehaviour {
+
+    [SerializeField]
+    private Material _material;
+    [SerializeField]
+    private float _speed;
+    private Vector2 _offset = new Vector2(0,1);
+
+    void Update () {
+        _material.mainTextureOffset += _offset * Time.deltaTime * _speed;
+	}
+}
