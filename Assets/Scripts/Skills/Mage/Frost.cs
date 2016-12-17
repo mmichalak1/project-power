010e4ebc03587ad8bd245ab985e7d7cb6d9b9df6
Mateusz <Mateusz@LAMALAP>
2016-12-17 15:04:10 +0100

    Fixed skill durations

--- Assets/Scripts/Skills/Mage/Frost.cs
+++ Assets/Scripts/Skills/Mage/Frost.cs
@@ -8,7 +8,6 @@ public class Frost : Skill
 
     public int StunDuration = 2;
     public StunnedBrain StunnedBrain;
-    private StunnedBrain _stunnedBrainCopy;
     public GameObject ParticleEffect;
 
     public override string Description()
@@ -18,29 +17,34 @@ public class Frost : Skill
 
     public override void Initialize(GameObject parent)
     {
-        _stunnedBrainCopy = Instantiate(StunnedBrain);
-        _stunnedBrainCopy.SetDuration(StunDuration);
         base.Initialize(parent);
     }
 
     public override void Initialize(EntityData data)
     {
-        _stunnedBrainCopy = Instantiate(StunnedBrain);
-        _stunnedBrainCopy.SetDuration(StunDuration);
         base.Initialize(data);
     }
 
     protected override void PerformAction(GameObject actor, GameObject target)
     {
-        GameObject go = Instantiate(ParticleEffect, target.transform.position + new Vector3(0, 0.25f, 0), Quaternion.identity) as GameObject;
-        go.transform.parent = target.transform;
-        _stunnedBrainCopy.ParticleEffect = go;
+       
         var attack = target.GetComponent<AttackController>();
         if (attack == null)
             return;
         Debug.Log(actor.name + " stuns " + target.name + " for " + (StunDuration) + " for turns.");
-        attack.AddBrain(_stunnedBrainCopy);
+        attack.AddBrain(CreateStunnedBrain(actor, target));
         base.PerformAction(actor, target);
+    }
+
+    private AbstractBrain CreateStunnedBrain(GameObject actor, GameObject target)
+    {
+        var copy = Instantiate(StunnedBrain);
+        copy.SetDuration(StunDuration);
+        GameObject go = Instantiate(ParticleEffect, target.transform.position + new Vector3(0, 0.25f, 0), Quaternion.identity) as GameObject;
+        go.transform.parent = target.transform;
+        copy.ParticleEffect = go;
+
+        return copy;
     }
 
 }
