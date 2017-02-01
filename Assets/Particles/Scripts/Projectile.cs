55b6d52d70491eede679d4bf085f827a17d96502
Marcin Mazur <marmaz.966@o2.pl>
2017-02-01 14:03:12 +0100

    Fireball reworked

--- Assets/Particles/Scripts/Projectile.cs
+++ Assets/Particles/Scripts/Projectile.cs
@@ -5,18 +5,24 @@ using System;
 public class Projectile : MonoBehaviour, IEffect {
 
     public float SpellDuration;
+    public float Delay;
 
     private float _speed;
     private Vector3 _direction;
+
 
     public void SetUpAction(GameObject actor, GameObject target)
     {
         transform.position += actor.transform.position + new Vector3(0, 0.5f, 0);
         _direction = target.transform.position - actor.transform.position;
-        _speed = _direction.magnitude / SpellDuration;
+        _speed = _direction.magnitude / (SpellDuration - Delay);
     }
 
 	void Update () {
-        transform.position += _direction.normalized * _speed * Time.deltaTime;
+        Delay -= Time.deltaTime;
+        if(Delay < 0)
+        {
+            transform.position += _direction.normalized * _speed * Time.deltaTime;
+        }
 	}
 }
---
+++ Assets/TriggerCameraEffect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerCameraEffect : MonoBehaviour {
+
+    public CameraShake cameraShake;
+
+    public float shakeAmount;
+    public float shakeDuration;
+
+	// Use this for initialization
+	void Start () {
+        cameraShake = GameObject.Find("CameraExploration").GetComponent<CameraShake>();
+        cameraShake.ShakeCamera(shakeAmount, shakeDuration);
+	}
+	
+}
