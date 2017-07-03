3d8d43f8c4d4b46625e368a0177d02c873cc196f
Mateusz <Mateusz@LAMALAP>
2017-07-03 17:34:07 +0200

    Created generation based on given tiles. few are missing

--- Assets/MapDecorator.cs
+++ Assets/MapDecorator.cs
@@ -1,60 +1,126 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class MapDecorator : MonoBehaviour {
+public class MapDecorator : MonoBehaviour
+{
 
     public MapGenerator generator;
+    public Vector3 StartingPoint;
+
+    public GameObject ErrorBlock;
+    public GameObject Map;
+
+    public List<GameObject> VerForwardBlocks;
+    public List<GameObject> HorForwardBlocks;
+    public List<GameObject> CrossingBlocks;
+    public List<GameObject> LeftForwardBlocks;
+    public List<GameObject> LeftBlocks;
+    public List<GameObject> RightForwardBlocks;
+    public List<GameObject> RightLeftBlocks;
+    public List<GameObject> RightBlocks;
+
+
 
 
     public void Decorate()
     {
+        Map.transform.position = StartingPoint;
         InstantiateAll();
     }
 
 
     private void InstantiateAll()
     {
-        InstantiateMainNodes();
-        InstantiateAdditionalNodes();
-        InstantiatePaths();
+        InstantiateNodes(generator.MainNodes);
+        InstantiateNodes(generator.AdditionalNodes);
+        InstantiatePaths(generator.Paths);
     }
 
 
 
-    private void InstantiateMainNodes(List<Node> MainNodes)
+    private void InstantiateNodes(List<Node> Node)
     {
-        for (int i = 0; i < MainNodes.Count; i++)
+        foreach (Node node in Node)
         {
-            GameObject go = Instantiate(TilePrefab, new Vector3(MainNodes[i].Position.x, 0.0f, MainNodes[i].Position.y), Quaternion.identity) as GameObject;
-            go.GetComponent<MeshRenderer>().material = Blue;
-
-            if (i == 0 || i == MainNodes.Count - 1)
-                go.GetComponent<MeshRenderer>().material = Gold;
+            GameObject TilePrefab = SelectPrefabForNode(node);
+            GameObject go = Instantiate(TilePrefab, new Vector3(node.Position.x, 0, node.Position.y), Quaternion.identity) as GameObject;
+            go.transform.SetParent(Map.transform, true);
+            go.transform.rotation = TilePrefab.transform.localRotation;
         }
 
 
-    }
-    private void InstantiateAdditionalNodes(List<Node> AdditionalNodes)
-    {
-        foreach (Node node in AdditionalNodes)
-        {
-            GameObject go = Instantiate(TilePrefab, new Vector3(node.Position.x, 0.0f, node.Position.y), Quaternion.identity) as GameObject;
-            go.GetComponent<MeshRenderer>().material = Red;
-        }
     }
     private void InstantiatePaths(List<Path> MainPaths)
     {
         GameObject go;
         foreach (Path path in MainPaths)
         {
+            GameObject TilePrefab;
+            if (path.source.Position.x == path.target.Position.x)
+                TilePrefab = HorForwardBlocks[0];
+            else
+                TilePrefab = VerForwardBlocks[0];
+
             foreach (Tile t in path.tiles)
             {
                 go = Instantiate(TilePrefab, new Vector3(t.Position.x, 0, t.Position.y), Quaternion.identity) as GameObject;
-                go.GetComponent<MeshRenderer>().material = Grey;
+                go.transform.SetParent(Map.transform);
             }
         }
     }
 
+    private void Update()
+    {
+        Debug.Log("Crap");
+    }
+
+
+
+    private GameObject SelectPrefabForNode(Node node)
+    {
+        bool right = node.Right != null;
+        bool left = node.Left != null;
+        bool up = node.Up != null;
+        bool down = node.Down != null;
+
+        if (down && up && left && right)
+        {
+            return CrossingBlocks[0];
+        }
+
+        if (down && left && up && !right)
+        {
+            return LeftForwardBlocks[0];
+        }
+
+        if (down && left && !right && !up)
+        {
+            return LeftBlocks[0];
+        }
+
+        if (up && right && down && !left)
+        {
+            return RightForwardBlocks[0];
+        }
+
+        if (right && left && down && !up)
+        {
+            return RightLeftBlocks[0];
+        }
+
+        if (down && right && !up && !left)
+        {
+            return RightBlocks[0];
+        }
+
+        if ((left || right) && !up && !down)
+            return HorForwardBlocks[0];
+
+        if ((up || down) && !right && !left)
+            return VerForwardBlocks[0];
+
+            return ErrorBlock;
+    }
 
 
 }
---
+++ Assets/Scripts/EditorScripts/Editor/ResetPositions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ResetPositions {
+
+    [MenuItem("Tools/RoundPositions")]
+	private static void RoundPostions()
+    {
+        Vector3 newPos;
+        Vector3 pos;
+        foreach (GameObject obj in Selection.gameObjects)
+        {
+            pos = obj.transform.position;
+            newPos = new Vector3(Mathf.RoundToInt(pos.x), 0.0f, Mathf.RoundToInt(pos.z));
+            obj.transform.position = newPos;
+        }
+    }
+}
