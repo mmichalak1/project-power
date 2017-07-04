c0dbeba39af9e9caa4ebedce5e8b490c0af79fe7
Mateusz <Mateusz@LAMALAP>
2017-07-04 18:41:33 +0200

    Writed tool for detecting middle point of block and set it as spawnPoint for new entities

--- Assets/Scripts/MapGeneration/BlockDataHolder.cs
+++ Assets/Scripts/MapGeneration/BlockDataHolder.cs
@@ -10,9 +10,11 @@ public class BlockDataHolder : MonoBehaviour {
     private List<GameObject> connectingTiles;
     [SerializeField]
     private List<BlockDataHolder> neighbouringBlocks;
+    [SerializeField]
+    private GameObject startingTile;
     
     public List<GameObject> PassableTiles { get { return passableTiles; } }
     public List<GameObject> ConnectingTiles { get { return connectingTiles; } }
     public List<BlockDataHolder> NeighbouringBlocks { get { return neighbouringBlocks; } }
-    public GameObject StartingTile { get; set; }
+    public GameObject StartingTile { get { return startingTile; } set { startingTile = value; } }
 }
