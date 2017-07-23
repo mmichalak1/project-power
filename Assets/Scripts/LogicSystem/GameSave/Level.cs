69ead5ebcffc31073099b0897925b7a4c071b6e8
Mateusz <Mateusz@LAMALAP>
2017-07-23 11:53:16 +0200

    Removed Chest data from GameSave

--- Assets/Scripts/LogicSystem/GameSave/Level.cs
+++ Assets/Scripts/LogicSystem/GameSave/Level.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Assets.LogicSystem.GameSave;
 
 
 public class Level
@@ -10,16 +9,11 @@ public class Level
     public static Level CreateFromRuntime(LevelData data)
     {
         Level result = new Level();
-        result.Chests = new List<Chest>();
 
         result.Progress = data.Progress;
         result.TargetProgress = data.TargetProgress;
         result.Visited = data.Visited;
         result.IsLocked = data.IsLocked;
-        foreach (var chest in data.Chests)
-        {
-            result.Chests.Add(Chest.CreateFromRuntime(chest));
-        }
         return result;
     }
 
@@ -29,17 +23,11 @@ public class Level
         level.TargetProgress = data.TargetProgress;
         level.Visited = data.Visited;
         level.IsLocked = data.IsLocked;
-        foreach (var chest in data.Chests)
-        {
-            var target = level.Chests.First(x => x.name == chest.Name);
-            Chest.CreateFromSavedData(ref target, chest);
-        }
     }
 
     public int Progress { get; set; }
     public int TargetProgress { get; set; }
     public int Visited { get; set; }
     public bool IsLocked { get; set; }
-    public List<Chest> Chests { get; set; }
 }
 
