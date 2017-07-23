249338e2373ef02591a58f8b4a0f44cc74ff3e27
Mateusz <Mateusz@LAMALAP>
2017-07-23 10:15:53 +0200

    Split player and enemies spawner

---
+++ Assets/ChestSpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ChestSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject ChestPrefab;
+    [SerializeField]
+    private IList<BlockDataHolder> chestsSpawns = new List<BlockDataHolder>();
+
+    public IList<BlockDataHolder> ChestsSpawns { get { return chestsSpawns; } }
+
+    public void SpawnChests()
+    {
+
+    }
+}
--- Assets/Scripts/Exploration/ChestScript.cs
+++ Assets/Scripts/Exploration/ChestScript.cs
@@ -1,58 +1,20 @@
 ﻿using System;
 using UnityEngine;
-using System.Collections;
 using System.Linq;
-using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class ChestScript : MonoBehaviour
 {
-
+    public int WoolForChest { get; set; }
     public GameObject Player;
     public GameObject WoolWindow;
     public SwipeManager Manager;
-    public ChestData ChestData;
     public GameSaverScript GameSaver;
-
-    public ExplorationHolder ExplorationHolder;
-    public Text TimerText;
-
     public bool IsCountingDown = false;
 
     void Start()
     {
-        if (ExplorationHolder.LevelPlayed == null)
-        {
-            GetComponent<Collider>().enabled = false;
-            Debug.Log("Current level played is not set, can't setup chest");
-            return;
-        }
-        ChestData = ExplorationHolder.LevelPlayed.Chests.First(x => x.name == gameObject.name);
-        var now = DateTime.UtcNow;
-        var diffrence = now - ChestData.LastOpened;
-        if (ChestData == default(ChestData) || diffrence < ChestData.Duration.ToTimeSpan())
-        {
-            Disable();
-        }
-        else
-        {
-            Enable();
-        }
-    }
-
-    void FixedUpdate()
-    {
-        if (!IsCountingDown)
-            return;
-
-        if (ChestData.Duration.ToTimeSpan() < DateTime.UtcNow - ChestData.LastOpened)
-        {
-            Enable();
-            return;
-        }
-
-        var span = ChestData.Duration.ToTimeSpan() - (DateTime.UtcNow - ChestData.LastOpened);
-        TimerText.text = TimeSpanToString(span);
+       
     }
 
     void OnTriggerEnter(Collider other)
@@ -60,30 +22,7 @@ public class ChestScript : MonoBehaviour
         if (other.gameObject != Player)
             return;
         WoolWindow.SetActive(true);
-        WoolWindow.GetComponent<AfterBattleScreen>().Show(ChestData.WoolForChest);
-        ChestData.LastOpened = DateTime.UtcNow;
-        Manager.enabled = false;
-        Disable();
+        WoolWindow.GetComponent<AfterBattleScreen>().OnEvoke(WoolForChest);
         GameSaver.SaveGame();
-    }
-
-    private void Disable()
-    {
-        TimerText.enabled = IsCountingDown = true;
-        GetComponent<Collider>().enabled = false;
-    }
-
-    private void Enable()
-    {
-        TimerText.enabled = IsCountingDown = false;
-        GetComponent<Collider>().enabled = true;
-    }
-
-
-    private static string TimeSpanToString(TimeSpan span)
-    {
-        return span.Hours.ToString("00") + ":" + 
-            span.Minutes.ToString("00") + ":" +
-            span.Seconds.ToString("00");
     }
 }
